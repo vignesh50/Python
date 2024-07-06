5e02a9fe218eb20d04ac3ee3e79c0c46e03ca6cb
vignesh50 <128451342+vignesh50@users.noreply.github.com>
2024-07-06 20:42:39 +0530

    Add files via upload

---
+++ Python/DAY 3/remainder.py
@@ -0,0 +1,12 @@
+print("Remind before leave home")
+
+user_in = input("Shall we start. Type Yes! or No! to exit \n").lower()
+
+if user_in == "yes":
+    print("Can you get your \n1. Wallet \n")
+    print("2. Ready Cloth \n")
+    print("3. Emergency Fund \n")
+    print("4. Mobile Battery Health \n")
+    # print("5.")
+else:
+    print("Ok, Everything is fine..Ready to Go..")
