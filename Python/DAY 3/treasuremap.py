550f8e39375a5fdf8107f5ae50f189453794a987
vignesh50 <128451342+vignesh50@users.noreply.github.com>
2024-07-08 12:56:55 +0530

    Add files via upload

---
+++ Python/DAY 3/treasuremap.py
@@ -0,0 +1,18 @@
+line1 = ["⬜","️⬜️","️⬜️"]
+line2 = ["⬜️","⬜️","️⬜️"]
+line3 = ["⬜️️","⬜️️","⬜️️"]
+map = [line1, line2, line3]
+print("Hiding your treasure! X marks the spot.")
+position = input() # Where do you want to put the treasure?
+# 🚨 Don't change the code above 👆
+# Write your code below this row 👇
+alpha = ['A','B','C']
+col = alpha.index(position[0])
+row = position[1]
+print(f"row: {int(row) -1 } and colun: {col}")
+
+map[int(row) -1][col] = "X"
+
+# Write your code above this row 👆
+# 🚨 Don't change the code below 👇
+print(f"{line1}\n{line2}\n{line3}")
