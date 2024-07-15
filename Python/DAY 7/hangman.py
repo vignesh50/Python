055eee1566dafcfc37b371e523862a8ececc2549
vignesh50 <128451342+vignesh50@users.noreply.github.com>
2024-07-15 21:36:19 +0530

    Add files via upload

--- Python/DAY 7/hangman.py
+++ Python/DAY 7/hangman.py
@@ -15,25 +15,35 @@ for x in range(lenOfWord):
     blank.append("_")
 
 
-def life():
+def life1():
     if life > 0:
         return True
     else:
         return False
 
-
-user_in = input("Guess the Letter: ")[0]
-print(f"you choose letter is : {user_in}")
-
-option = int(input("Do you have life: 0 is out / 1 is In: "))
-
+print(f"your life has: {life}")
+option = True
 while option:
-# to Find position
-    for x in range(len(pc_choose)):
-        if user_in == pc_choose[x]:
-            print(f"index position is: {x}")
-            blank[x] = user_in
+    user_in = input("Guess the Letter: ")[0].lower()
+    print(f"you choose letter is : {user_in}")
+    # print(f"your life has: {life}")
 
+
+# user choose word is present in pc choose word or not
+    if user_in in pc_choose:
+        # to Find position
+        for x in range(len(pc_choose)):
+            if user_in == pc_choose[x]:
+                # print(f"index position is: {x}")
+                blank[x] = user_in
+        print(f"Filled: {blank}")
+        print(f"your life left: {life}")
+       # option = int(input("Do you have life: 0 is out / 1 is In: "))
+    else:
+        life -= 1
+        print(f"Oh No!! \nyour life left: {life}")
+    if life < 0:
+        option = False
 
 print(f"Final result: {blank}")
 
